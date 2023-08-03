9cc413dfcd32aa887a4ee134918adf9ee98a549e
Pip Austin <pippa.austin@live.co.uk>
2023-08-03 07:07:05 +0100

    Added banners json

---
+++ bannersJson.js
@@ -0,0 +1,3 @@
+const bannersJson = [
+    {name: "VBTS Red", imgPlain: "plain.png", imgOverlay: "overlay.png"},
+    {name: "VBTS Grey", imgPlain: "overlayGrey.png", imgOverlay: "overlayGrey.png"}];
