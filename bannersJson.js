322423848ef3e5cd2c31872b90921ace62739c3c
pip austin <philippa.austin@vodafone.com>
2023-08-14 08:45:49 +0100

    Added new img overlay with white border

--- bannersJson.js
+++ bannersJson.js
@@ -1,3 +1,3 @@
 const bannersJson = [
-    {name: "VBTS Red", imgPlain: "plain.png", imgOverlay: "overlay.png"},
+    {name: "VBTS Red", imgPlain: "overlayNew.png", imgOverlay: "overlayNew.png"},
     {name: "VBTS Grey", imgPlain: "overlayGrey.png", imgOverlay: "overlayGrey.png"}];
