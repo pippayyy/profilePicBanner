4ed6a25ed122ecb985e2fee11749f91b2792710b
Pip Austin <pippa.austin@live.co.uk>
2023-09-01 10:01:05 +0100

    New banners added

--- bannersJson.js
+++ bannersJson.js
@@ -1,3 +1,3 @@
 const bannersJson = [
-    {name: "VBTS Red", imgPlain: "overlayNew.png", imgOverlay: "overlayNew.png"},
-    {name: "VBTS Grey", imgPlain: "overlayGrey.png", imgOverlay: "overlayGrey.png"}];
+    {name: "VBTS Red", imgPlain: "redVBTSBanner.png", imgOverlay: "redVBTSBanner.png"},
+    {name: "VBTS Grey", imgPlain: "blackVBTSBanner.png", imgOverlay: "blackVBTSBanner.png"}];
